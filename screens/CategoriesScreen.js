6c6fd3060d8d7a77c4b460bb63a24e12916ab010
LeuzziLuigi <leuzzi.luigi98@gmail.com>
2020-04-14 10:34:50 -0500

    Update CategoriesScreen.js
    refactoring

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -6,7 +6,9 @@ const CategoriesScreen = props => {
     return (
         <View style={styles.screen}>
             <Text>The Categories Screen!</Text>
-            <Button title="Go To Meals!" onPress={() => {}} />
+            <Button title="Go To Meals!" onPress={() => {
+                props.navigation.navigate('CategoryMeals');
+            }} />
         </View>
     )
 }
