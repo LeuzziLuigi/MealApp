85384e0a57117900c3c28c995f4db3d570d47d3a
LeuzziLuigi <leuzzi.luigi98@gmail.com>
2020-04-15 10:07:54 -0500

    pause
    this stuff seems outdated

--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -10,7 +10,12 @@ const CategoriesScreen = props => {
 
     const renderGridItem = itemData => {
         return (
-            <TouchableOpacity style={styles.gridItem} onPress={() => { props.navigation.navigate('CategoryMeals')}}>
+            <TouchableOpacity 
+            style={styles.gridItem} 
+            onPress={() => {
+                props.navigation.navigate('CategoryMeals', {categoryId: itemData.item.id})
+                }}
+            >
                 <View>
                     <Text>{itemData.item.title}</Text>
                 </View>
--- screens/CategoryMealsScreen.js
+++ screens/CategoryMealsScreen.js
@@ -2,10 +2,17 @@ import React from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 import CategoriesScreen from './CategoriesScreen';
 
+import {CATEGORIES} from '../data/dummy-data';
+
 const CategoryMealsScreen = props => {
+    const catId = props.navigation.getParam('categoryId');
+
+    const selectedCategory = CATEGORIES.find(cat => cat.id === catId);
+
     return (
         <View style={styles.screen}>
             <Text>The Category Meals Screen!</Text>
+            <Text>{selectedCategory.title}</Text>
             <Button title="Go To Meals Detail!" onPress={() => {
                 props.navigation.navigate('MealsDetail');
             }} />
